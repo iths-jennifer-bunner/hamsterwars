83de3c170ca4862cdb08efb8e6ec320864b16d5c
iths-jennifer-bunner <54809580+iths-jennifer-bunner@users.noreply.github.com>
2020-05-13 15:28:28 +0200

    added function to shorten Id

--- routes/games.js
+++ routes/games.js
@@ -6,8 +6,9 @@ const router = new Router();
 //Save a new match
 router.post('/', async (req, res) => {
     try{
+        let id = generateId(3)
         let date = new Date().toUTCString()
-        let newGames = await db.collection('games').doc()
+        let newGames = await db.collection('games').doc(id)
 
         await newGames.set({
             id: newGames.id,
@@ -34,7 +35,18 @@ router.get('/', async (req, res) => {
     catch(err){console.error(err)}
 })
 
+//----------------------------//
+function generateId(length){
 
+    let id = '';
+    let chars = 'abcdefghijklmnopqrstuvxyz0123456789';
+
+    for(let i= 0; i < length; i++ ){
+        let rand = Math.floor(Math.random()*chars.length);
+        id += chars[rand];
+    }
+    return id;
+}
 
 
 module.exports = router;
